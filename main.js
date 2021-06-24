9c4ce2cbf7fd959d8f65f668b8b302f93c6370ae
Athouba-Pebam <71960090+Athouba-Pebam@users.noreply.github.com>
2021-06-24 16:46:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+     canvas = createCanvas(550, 500);
+     canvas.position(620, 100);
+
+     poseNet = ml5.poseNet(video, modelLoded);
+     poseNet.on('pose', gotPoses);
+  
+}
+
+function modelLoded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+   {
+       console.log(results);
+       noseX = results[0].pose.nose.x;
+       noseY = results[0].pose.nose.y;
+       console.log("noseX = " + noseX + " noseY = " + noseY);
+
+       leftWristX = results[0].pose.leftWrist.x;
+       rightWristX = results[0].pose.rightWrist.x;
+       difference = floor(leftWristX - rightWristX);
+
+       console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+
+   } 
+}
+
+function draw(){
+    background("#0000FF");
+
+    document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference + "px";
+    fill('#00FF00');
+    stroke('#00FF00');
+    square(noseX, noseY, difference);
+}
